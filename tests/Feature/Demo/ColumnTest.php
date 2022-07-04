d3c5d47d34f83e5619ee628277c0a5e5a9ede9dd
wxyClark <C18666211369@outlook.com>
2022-07-04 09:39:52 +0800

    测试demo

--- tests/Feature/Demo/ColumnTest.php
+++ tests/Feature/Demo/ColumnTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Demo;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Services\BaseService;
+use App\Services\MakeSample\MakeSampleOrderPaymentService;
 use Tests\TestCase;
 
 /**
@@ -11,15 +11,47 @@ use Tests\TestCase;
  */
 class ColumnTest extends TestCase
 {
+    /** @var BaseService */
+    private $service;
+
+    private $loginUser = [
+        'tenant_id' => '租户ID',
+        'company_code' => '企业编码',//  雪花ID，全局唯一  一个租户下可以有多家企业
+        'company_name' => '企业名称',
+        'user_code' => '用户编码',  //  雪花ID，全局唯一
+        'user_name' => '用户名称',
+    ];
+
+    public function __construct(BaseService $service)
+    {
+        $this->service = $service;
+    }
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_undefined_key()
+    public function testControllerName()
     {
-        $response = $this->get('/');
+        $response = $this->actionAName();
+//        $response = $this->actionBName();
 
-        $response->assertStatus(200);
+        dd($response);
+    }
+
+    /**
+     * @desc    controller的一个action 对应这里的一个私有方法
+     * @return mixed
+     * @author  wxy
+     * @ctime   2022/7/4 9:35
+     */
+    private function actionAName()
+    {
+        $params = [];
+        $rules = [];
+        $this->baseValidate($params, $rules);
+
+        return $this->service->funcA($this->loginUser, $params);
     }
 }
